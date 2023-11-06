d199c9ff8f121abf875a4c369811753283bb5a42
wichapas <wpichetp@ucsc.edu>
2023-11-05 16:10:00 -0800

    fishing game update 11/5/2023

--- docs/_cmpm170/main.js
+++ docs/_cmpm170/main.js
@@ -1,83 +1,128 @@
 title = "Harry's Game";
 
-description = ``;
+description = `test test`;
+
+let pins;
+let pins2;
+let pins3;
+let characterY = 95;
+let characterX = 74;
+let lifePoint = 10;
+let nextFishDist;
+let fishLine;
+let lineLength = 5;
+let projectileObj;
+let retract = false;
+let caughtFish = false;
+let fishSpeed = 1;
+let retractSpeed = 1;
 
 characters = [
 `
-   l
-  lll
- lllll
- rgrgr
- rrgrr
- rrrrr 
+ccccc
+c   c
+c   c
+ccccc
+cc cc
+cc cc
+`,
+`
+bbbbb
+lllll
+blllb
+llbll
 `,
 ];
 
 options = {};
 
-let shapes
-
-let pins;
-let rodPin
-let rod;
-let powerBar;
-let mainCharacter;
-let nextPinDist;
-const rodLength = 7;
-let powerBarLength = 25;
-
 function update() {
   if (!ticks) {
-    pins = [vec(5,50)]
-    rodPin = [vec]
-    nextPinDist = 15;
-    rod = {angle: 0, length: rodLength, pin: vec(50, 70)}
-    powerBar = {angle: 140, length: powerBarLength, pin: vec(10, 70)}
+    pins = [vec(5, 50)];
+    pins2 = vec(50, 70)
+    pins3 = [vec(95, 50)]
+    fishLine = {angle: 110, length: lineLength, pin: vec(53, 70)}
+    projectileObj = {pin: vec(50, 70), color: "transparent"}
+
+    nextFishDist = 100;
+
   }
-  mainCharacter = char("a", 50, 95);  //50 for middle page 95 for bottom screen
-  // arc(40, 95, 30, 3, 0, -1)
-
-
-  let mouse = input.pos
-
-  if (input.isJustPressed) {
-    rod.length += 50;
-    //rod.pi = input.pos;
-  } else {
-    rod.length += (rodLength - rod.length) * 0.1;
-  }
-
-  // if()
- 
-  rod.angle += 0.05
+  color("cyan");
+  char("a", characterX, characterY);
+  color("black");
+  arc(40, 95, 30, 3, 0, -1)
+  line(fishLine.pin, vec(fishLine.pin).addWithAngle(fishLine.angle, fishLine.length));
+  color("green");
+  rect(projectileObj.pin, 3)
   
+
   
-  line(rod.pin, vec(rod.pin).addWithAngle(rod.angle, rod.length));
- 
-  // line(powerBar.pin, vec(powerBar.pin).addWithAngle(powerBar.angle, powerBar.length));
-
-// if(fished == false && powerSet == false){
-  // if(input.isPressed){
-  //   powerBar.length -=1
-  //   if(powerBar.length <= 0){
-  //     powerBar.length = 25;
-  //   }
-  // }
 
 
-  let distanceX = 0.2;
   remove(pins, (p) => {
-    p.x += distanceX;
-    box(p, 3);
-    if(box(p,3).isColliding.rect.black && p != rod.pin){
-      console.log("ddds")
+    p.x += fishSpeed
+    color("yellow");
+    if(box(p, 3).isColliding.rect.green){
+      caughtFish = true;
+      fishSpeed = 0;
+      if(input.isJustPressed){
+        fishLine.angle += 0.5;
+        projectileObj.pin.y += retractSpeed
+        if(p.y+0.5 < projectileObj.pin.y){
+          p.y += retractSpeed;
+        }
+      }
+      else{
+        p.x += 0.005;  
+      }
+      if(p.x-4.5 > projectileObj.pin.x){
+        caughtFish = false;
+        fishSpeed = 1;
+      }
+      if(projectileObj.pin.y == pins2.y){
+        addScore(1);
+        caughtFish = false;
+        fishSpeed = 1;
+        
+        return projectileObj.pin.y == pins2.y;
+      }
     }
-    return p.x > 102;
+    
+    let outOfBoundX = 102
+    if(p.x > outOfBoundX){ 
+      lifePoint -= 1;
+      console.log(lifePoint);
+      if(lifePoint == 0){
+        lifePoint = 10;
+        end("Game Over");
+      }
+    }
+    return p.x > outOfBoundX;
   });
 
-  nextPinDist -= distanceX;
-  while (nextPinDist < 0) {
-    pins.push(vec(-2 - nextPinDist, rnd(10, 60)));
-    nextPinDist += rnd(20, 50);
+  let projectileSpeed = 2; 
+    
+  if(input.isPressed && retract == false && caughtFish == false){
+    if(projectileObj.pin.y > 10 ){
+      projectileObj.pin.y -= projectileSpeed;
+    }
+  }
+  else{
+    retract = true; 
+    if(projectileObj.pin.y < pins2.y && caughtFish == false){
+      fishLine.angle += 0.5;
+      projectileObj.pin.y += 1;  
+    }
+    if(projectileObj.pin.y >= pins2.y){
+      retract = false;
+    }
+  }
+
+  nextFishDist -= fishSpeed;
+  if(nextFishDist < 0){
+    pins.push(vec(5, rnd(10,60)));
+    nextFishDist += rnd(50, 100);
   }
 }
+
+// function projectile
